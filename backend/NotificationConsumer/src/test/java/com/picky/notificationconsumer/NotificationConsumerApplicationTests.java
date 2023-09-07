aaa4b508e906eef1dc5b3d02572ba4c6c3e2271d
김기헌 <cbj2741@naver.com>
2023-09-07 13:24:14 +0900

    chore: yml파일 mysql 연동 및 jpa 설정
    Former-commit-id: 525c95a7ef0acbf72df14a5a4573582d3e6756ae

--- backend/NotificationConsumer/src/test/java/com/picky/notificationconsumer/NotificationConsumerApplicationTests.java
+++ backend/NotificationConsumer/src/test/java/com/picky/notificationconsumer/NotificationConsumerApplicationTests.java
@@ -1,3 +1,11 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:d754d48725921aad04cb48b0837e1ab6e870abf685409d838c525dbbe02959f8
-size 204
+package com.picky.notificationconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationConsumerApplicationTests {
+
+
+
+}
