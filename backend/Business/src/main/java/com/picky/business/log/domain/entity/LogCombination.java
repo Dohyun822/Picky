5ec7d3dccd24267be0d492cffd9a69cf1bcf69b7
김기헌 <cbj2741@naver.com>
2023-09-08 17:50:13 +0900

    feat: Product Create, Read 구현중
    Former-commit-id: fcc55d7b9c0d170bc935d582fc143d1ad1a20a7a

--- backend/Business/src/main/java/com/picky/business/log/domain/entity/LogCombination.java
+++ backend/Business/src/main/java/com/picky/business/log/domain/entity/LogCombination.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:824725cd865927a0a01e2e1262107fc9c1380de4eb695328fdc4978c4c6114e2
-size 754
+oid sha256:65f6636763072c16d762420f915ef8c0117fd60d81ab55bf2b8f5a3a7b7b07f9
+size 838
--- backend/Business/src/main/java/com/picky/business/product/domain/entity/Badge.java
+++ backend/Business/src/main/java/com/picky/business/product/domain/entity/Badge.java
@@ -1,6 +1,3 @@
-package com.picky.business.product.domain.entity;
-
-public enum Badge {
-    ONE_PLUS_ONE,
-    TWO_PLUS_ONE
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:4c75cdac0f5b035228705bc61174a3056b4c0728b8563de838c1c66b1ea5f496
+size 576
