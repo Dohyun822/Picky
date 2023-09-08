54016b1f0d0cf035b18e795c08b55193426eb1c6
imsongj <imsongj@gmail.com>
2023-09-08 17:47:43 +0900

    FEAT: vertical_more_list 수정 중, 카드 수정 필요
    Former-commit-id: c776af80c9ca421562f1ba48ebbe939aa12e5af6

--- frontend/lib/pages/home_page.dart
+++ frontend/lib/pages/home_page.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2e75837dda56d4d0156e0b857dfa39f1e23cef30cfa45ba29477ada1b3259d12
-size 1487
+oid sha256:1d5520da6a3cb034e8de6248e538aa8bc71a0367b4f84bd7d3326a7d26d574b0
+size 1047
