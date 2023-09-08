c84a4cf03bdc6708088714005fae680b1a144b96
imsongj <imsongj@gmail.com>
2023-09-08 17:10:53 +0900

    FEAT: 라우터 수정
    Former-commit-id: 6078e43136b07c7518f56514cf0623f0441ec908

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:40ab1e65e986108370f837da7ab3fc4cff2bc420083efd9fadb2cc91634f60d6
+oid sha256:98a47347371210e5f175815269c0e8ffabcb73b9c4590d6a04f0c8577443dbfc
 size 3733
--- frontend/lib/pages/home_page.dart
+++ frontend/lib/pages/home_page.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:82dd572b343e32dc4067ccea189cee1a2ef9440f5997edeef69ffcf3fd662a77
-size 1536
+oid sha256:2e75837dda56d4d0156e0b857dfa39f1e23cef30cfa45ba29477ada1b3259d12
+size 1487
