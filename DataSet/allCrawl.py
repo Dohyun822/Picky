e668f6f3dd94148401944c08dce312ed6f31f336
임준성 <jsbryan@naver.com>
2023-09-08 11:34:10 +0900

    ✨ CU 단일 품목 크롤링 기능 추가, 자동 스크롤 기능 추가, find_review 함수명 변경
    Former-commit-id: 62a07c786fca2309bf7f03852a415225a9388d95

--- DataSet/allCrawl.py
+++ DataSet/allCrawl.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e2a1ca09c288a3fefffd3bca2b0f7c2f455431f18b82596d08e1d6457b867f65
-size 4825
+oid sha256:95f974db6433f801f9a981a1bfda1c69bcce21dd30a811ab85ce2e09c7d4b6f9
+size 4865
--- DataSet/cuCrawl.py
+++ DataSet/cuCrawl.py
@@ -1,1 +1,3 @@
-print(123)
+version https://git-lfs.github.com/spec/v1
+oid sha256:5ac8aff3fdcfa87f6e9a3de78d47b7ed032d094e99a071f8a451080376191242
+size 5296
