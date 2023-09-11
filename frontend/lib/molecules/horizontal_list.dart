0a8247da26a571dc867e03e6b3cff7f0b091caa7
imsongj <imsongj@gmail.com>
2023-09-11 17:15:15 +0900

    FEAT: padding 수정, 스크롤 수정
    Former-commit-id: fa08c2c63519346eef270bb4fa63c675a7576c7e

--- frontend/lib/molecules/horizontal_list.dart
+++ frontend/lib/molecules/horizontal_list.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:28457c962881f3336a9772f8bfb210ddf0eb02f9e74154e6e608a63ddcf2f5c3
-size 1195
+oid sha256:afd2b6d18248b004c6e5ae0e0231e832bf931aa03e48ab2289e31abb890cfc6d
+size 1298
--- frontend/lib/molecules/ranking.dart
+++ frontend/lib/molecules/ranking.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8f64970cade81e44c7d7f9ebd4d244f5e5106e97b1a7b6243ede69333144cb02
-size 2156
+oid sha256:6aa05104f313293e744a58d60ab6233fec3d1cc4625c2ff05bd1f3c69d7f18c7
+size 2274
