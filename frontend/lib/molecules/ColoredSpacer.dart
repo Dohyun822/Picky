b07f4f7cdbf980e5474f7f3f6d76938b45744003
imsongj <imsongj@gmail.com>
2023-09-11 15:26:18 +0900

    FEAT: 랭킹 디자인 수정
    Former-commit-id: 030ff423e6d9570d2c5ab2b7e70bae5168b091a3

---
+++ frontend/lib/molecules/ColoredSpacer.dart
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:02a3e11add04c7bf0c524a9b5d8b367e297a64d4c8fa38829a87a5153653fd31
+size 254
--- frontend/lib/pages/home_page.dart
+++ frontend/lib/pages/home_page.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:def9e2a99d456bc85e6a4e678893ae40a9e5e15f24d4b1cce9f2067c746c4d44
-size 1392
+oid sha256:18a79d065df2cef2a67ae99f30f8b8f82f4550a038c2b7e851dde221548e2a56
+size 1321
