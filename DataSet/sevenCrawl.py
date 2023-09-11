8a977a3346eb798061a9a33def3c77cfdd11abb6
임준성 <jsbryan@naver.com>
2023-09-11 15:06:59 +0900

    ✨ SevenEleven 크롤링 기능 완성
    Former-commit-id: e25638a9f1cf1f5ca88ba56515138e68f7910e97

--- DataSet/sevenCrawl.py
+++ DataSet/sevenCrawl.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:346f0f8ea45e74c9b8f2940999d9373d9b4f8ccb0fe2ef823fdd104623544022
-size 10
+oid sha256:ac0657e5f1ee948851fffb20c85ce23c11074fb99c9997bff0ebf63dc6dfef17
+size 6364
