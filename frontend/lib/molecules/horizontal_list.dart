3eee84f6e69f44fca8c82503afd6edae66834da4
imsongj <imsongj@gmail.com>
2023-09-11 16:19:51 +0900

    FEAT: HorizontalList 수정
    Former-commit-id: b4dde165c9ba9c405e06e58f46eba24c085217ec

--- frontend/lib/molecules/horizontal_list.dart
+++ frontend/lib/molecules/horizontal_list.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:59a7038686b8ea04cca9c4f3459811c29cad9919acda766cd2823c133cbed5ef
-size 552
+oid sha256:dc4e081e11b8adedfa92db5783eeba6542661a20edb43a4e12cfdc30ab9aac0e
+size 1006
