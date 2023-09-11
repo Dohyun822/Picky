bdd48cdec2c78fbcd65632128e83ac483647bcf2
김기헌 <cbj2741@naver.com>
2023-09-11 14:12:44 +0900

    feat: Product Create, Read 완료, Badge -> String 변경
    Former-commit-id: 34dc8c738f4b46bcea96394081431d2b92896d6d

--- backend/Business/src/main/java/com/picky/business/product/domain/entity/Comment.java
+++ backend/Business/src/main/java/com/picky/business/product/domain/entity/Comment.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8074a2767ec077fd1aa52fc51bcb7dd1172f394902ce136777c27da400382ea5
-size 742
+oid sha256:c422796a1fb44e4addc12bbe892c78827096b21cd414d04d5984104212d134ca
+size 721
--- backend/Business/src/main/java/com/picky/business/product/domain/entity/Product.java
+++ backend/Business/src/main/java/com/picky/business/product/domain/entity/Product.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bfed0a743058386dbcb9d6c42257afd3adc1e7c06235abef0ffa4dee385370c6
-size 1061
+oid sha256:637e90f9dec8c505dd7690453cce5d00c5de34df7592db6ad17d3bba746ab28f
+size 1188
