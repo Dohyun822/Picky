7aac32963f0318793fa3aea2138d2d7b64262f36
임준성 <jsbryan@naver.com>
2023-09-11 15:36:39 +0900

    ✨ emart 크롤링 기능 페이징 처리 제외 완성
    Former-commit-id: 9a1fcd15879c52e190bd75f6f2b6fc9061d76446

--- DataSet/emartCrawl.py
+++ DataSet/emartCrawl.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:346f0f8ea45e74c9b8f2940999d9373d9b4f8ccb0fe2ef823fdd104623544022
-size 10
+oid sha256:8d5802fcda9136c04ad3427b6a7211f186eb9d211732455e942d7b56842d120c
+size 5090
