be1e82a033ae92efe28e4911772653cf75b40c3b
imsongj <imsongj@gmail.com>
2023-09-12 13:11:07 +0900

    feat: 추천 리스트 이름 수정
    Former-commit-id: a752d0eca5036809b7c37bd383e60dc4579e9952

--- frontend/lib/pages/product_detail_page.dart
+++ frontend/lib/pages/product_detail_page.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6f42c5893934cc1fc11fa9e9ddf1ebede7198a3beba7f4e4c208b4421968b052
-size 12912
+oid sha256:eeb3e3defe7942c0900aef4daed4592620ff3fdf2bec7c0d2733c976aa06e3a0
+size 13484
