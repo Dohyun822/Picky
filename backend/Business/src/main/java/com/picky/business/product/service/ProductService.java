802708554cd86bd340b4f345f283d7856ac8e596
김기헌 <cbj2741@naver.com>
2023-09-12 17:56:43 +0900

    chore: redis 셋팅
    Former-commit-id: e7dd4b34977703a3926201a84e4cee181a6d72bc

--- backend/Business/src/main/java/com/picky/business/product/service/ProductService.java
+++ backend/Business/src/main/java/com/picky/business/product/service/ProductService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d207c42a769ecdb9417b8b8b4b22af928c2a86b9884f31fbac0bba66bb1d5c1c
-size 5238
+oid sha256:290d6570154ae17304fa64b4fe937353497bd40f4bbdad13b48d40dd3881e692
+size 5239
