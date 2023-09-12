136c98f74e0c198c5d871b1df4de87a2d86dfb6c
flowerdonk <dbwls5140@gmail.com>
2023-09-12 11:04:26 +0900

    feat: 로그아웃, 회원탈퇴 get~byJWT 기능 추가
    Former-commit-id: edee499f237bfabbca507341837a4c8bdfd4000f

--- backend/Auth/src/main/java/com/picky/auth/user/service/AuthService.java
+++ backend/Auth/src/main/java/com/picky/auth/user/service/AuthService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:63ce2b55ff83af66de37d169d873b70f1ecd3db47a9f567200e34c81fe9f222b
-size 2846
+oid sha256:cbcfc99d8395225475ca98758668bb55c84365e977e29f60304ce4426d72f68a
+size 4215
