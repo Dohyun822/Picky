9c63556403ebbdbc84e4e2be59988c5caa7661df
flowerdonk <dbwls5140@gmail.com>
2023-09-13 17:34:12 +0900

    feat: 시큐리티 설정 추가
    Former-commit-id: b590d46269b1702fea97beaa6aff63019a0c61c0

--- backend/Auth/src/main/java/com/picky/auth/config/security/SecurityConfig.java
+++ backend/Auth/src/main/java/com/picky/auth/config/security/SecurityConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5018390e3351a76a343ebcd33f0071e657db06c1052a76ba38d6fefeb479ab21
-size 2468
+oid sha256:3fe0f57703b37db77f3af6a5d7deb40217cb908a4e1171ff9d1c763a78fb0d03
+size 2665
