d08490bb5d1e221a465bafeb6fd9e5b2ca8e0470
imsongj <imsongj@gmail.com>
2023-09-13 15:41:07 +0900

    refactor: 파일 이름 수정
    Former-commit-id: a2af67933d86bc3dec27b6347e57fc7cd1aaa871

--- frontend/lib/atom/product_card.dart
+++ frontend/lib/atom/product_card.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ae84d90ae2a3d1be3fee137dd2f89f2fe7075c7d984961ac30db8c0996924044
-size 1604
+oid sha256:756237d882b76105dae6d056841488067ff50472b2a7cee2993d84dd31c24274
+size 1608
