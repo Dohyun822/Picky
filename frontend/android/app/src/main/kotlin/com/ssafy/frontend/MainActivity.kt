a5d37410bad3e1acde2fa373065a88865ca04f29
imsongj <imsongj@gmail.com>
2023-09-13 14:52:08 +0900

    feat: 장바구니 삭제 로직 수정
    Former-commit-id: 60c06b89b782519ea634f12c2bbc3f1dcb9b3de2

--- frontend/android/app/src/main/kotlin/com/ssafy/frontend/MainActivity.kt
+++ frontend/android/app/src/main/kotlin/com/ssafy/frontend/MainActivity.kt
@@ -1,3 +1,6 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:aa2efa8b9ffcca7f4ba4beee3031ccf4d64d4e3ffa3737f30accbef69b0d6f45
-size 123
+package com.ssafy.frontend
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- frontend/lib/pages/cart_page.dart
+++ frontend/lib/pages/cart_page.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:95ee68b27686e8d3af494171107a24a0772863ac99a084af99a7b93945ec059c
-size 3280
+oid sha256:e84676b4a6c418352cf256d3cdef2ac4bddeebf10e7de377254920c59492602f
+size 3415
