bb6c959d652121a5a144ae217ed9fa22dc1d19ce
jeongmo <jeremy0410@hanmail.net>
2023-09-13 10:39:41 +0900

    feat : alarm button bug fix & temp detail with chart
    Former-commit-id: 3b5834cd61745bd549aefdfaddc46597417428de

--- frontend/lib/pages/product_detail_page.dart
+++ frontend/lib/pages/product_detail_page.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eeb3e3defe7942c0900aef4daed4592620ff3fdf2bec7c0d2733c976aa06e3a0
-size 13484
+oid sha256:05b655944d5e9c12a344d2f519a300db7c0183f6d7aa14057d80a12a6dc98fe5
+size 14241
