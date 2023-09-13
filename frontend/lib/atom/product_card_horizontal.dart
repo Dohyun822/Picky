82e74cd40076586fe1e52fc43ca86468123d13fd
imsongj <imsongj@gmail.com>
2023-09-13 14:45:58 +0900

    feat: 삭제 확인 창 추가
    Former-commit-id: 096a500dedb931652e7e670133f508c0fd465392

--- frontend/lib/atom/product_card_horizontal.dart
+++ frontend/lib/atom/product_card_horizontal.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a220a89053671f953076d3769b0fd8d08c5d59cc68a5cacbd5cdc2a221cc3972
-size 1690
+oid sha256:2240abe36c0da359a414b2af3f5679e35181b42761dff41f7c349cb4b39da9ab
+size 2038
---
+++ frontend/lib/molecules/CartConfirmRemoveDialog.dart
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:664f61af85a04afedb52ba0c481b1b0d6c5ea8d4abb9eca80957209968f86c92
+size 862
