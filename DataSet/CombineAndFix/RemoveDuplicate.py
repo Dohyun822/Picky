0af063da5cb0d0946e6060caac2569a751d19ef9
flowerdonk <dbwls5140@gmail.com>
2023-09-14 16:16:31 +0900

    feat: kafka producer 설정 추가
    Former-commit-id: efd7b72082310c16c3075b9fcab73a9f29917975

--- DataSet/CombineAndFix/RemoveDuplicate.py
+++ DataSet/CombineAndFix/RemoveDuplicate.py
@@ -1,3 +1,9 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:6b02199056315ca25d654ece22dbe19572384d99f6ca441a4491560308693365
-size 422
+import pandas as pd
+
+# productDB.csv 파일 읽어오기
+df = pd.read_csv("productDBcsv.csv", encoding='cp949')
+# 중복된 상품명과 중량 제거
+df.drop_duplicates(subset=['상품명', '중량','칼로리'], keep='first', inplace=True)
+# 결과를 파일로 저장 (productNoDuplicated.csv)
+df.to_csv('productNoDuplicated.csv', index=False, encoding='cp949')
+print("productNoDuplicated.csv 파일이 생성 완료.")
---
+++ backend/NotificationProducer/src/main/java/com/picky/notificationproducer/config/KafkaProducerConfig.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7ceb6c065a8316cb305b796ded1ba539a4a2f4ebf0dd9429796a515cd4a73372
+size 1836
