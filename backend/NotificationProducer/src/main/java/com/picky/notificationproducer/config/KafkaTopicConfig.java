d2f86a13ca472c18eb39510e17d11176ea4b14b3
flowerdonk <dbwls5140@gmail.com>
2023-09-14 16:16:31 +0900

    feat: kafka producer 설정 추가
    Former-commit-id: ceae233a660073c526d325142c869e51df487fff

---
+++ backend/NotificationProducer/src/main/java/com/picky/notificationproducer/config/KafkaTopicConfig.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:fbd6e894ce39d72c6ed3527bf8be3953c12d31ee2eb0ec6876f51d6dc877fd3c
+size 1015
---
+++ backend/NotificationProducer/src/main/java/com/picky/notificationproducer/producer/service/ProducerService.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ce0fe7ec3f7a5b117803221177a528dc615b7071a7c146d8a9ac8fd3bbfa178e
+size 708
