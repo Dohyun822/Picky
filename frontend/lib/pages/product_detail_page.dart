86e861503daccb8da353f48eb2434887fed26646
jeongmo <jeremy0410@hanmail.net>
2023-09-14 10:14:28 +0900

    feat : chart temp, temp_in_all fin with no design
    Former-commit-id: 5a326e5108c9a323e3cdf5350c25d4bfa64813f0

--- frontend/lib/pages/product_detail_page.dart
+++ frontend/lib/pages/product_detail_page.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:37c077da6bd22b88b593064e2f505b96e9c43297e6a55b9997240fb422fa9b4c
-size 14782
+oid sha256:1278d8f3d16078a27ba892fb597307bec239a62a6a3a3b56e7c55a7b177210e2
+size 6676
