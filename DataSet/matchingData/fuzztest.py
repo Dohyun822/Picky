6ee2781ad2fb2d0d87fbec4333ee64d12032733a
임준성 <jsbryan@naver.com>
2023-09-14 15:48:23 +0900

    ✨ 토큰화 하여 Similarity를 판별하고 특정 수치 이상일 때 상품명을 저장하는 로직
    Former-commit-id: 90ab166833385be7e25cd752769574367dadfa67

--- DataSet/matchingData/fuzztest.py
+++ DataSet/matchingData/fuzztest.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:85a5733f9ab99322521fef9e52f9e84a7e3da0b3b66602b6807d645c07181101
-size 1164
+oid sha256:94d422ece7518716fdd2cc3b2107407215625db4595a58fba7556f77cfbca2c6
+size 2593
