33d92c5caf31f7af3190a19625938c7c9a3ec93b
imsongj <imsongj@gmail.com>
2023-09-18 16:42:29 +0900

    feat: 필터 provider 기능 추가
    Former-commit-id: 443af05ae195d270f0167d146b189f17a581109a

--- frontend/lib/models/filter.dart
+++ frontend/lib/models/filter.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:66d87c17f6ec9ad864f5963233e7664a7ecb87854260e2c34cd09052ad3e3d1f
-size 142
+oid sha256:010f148b042144e6b06f7e71f0276c833c767f2a1d5de794ae8507c3127b7a81
+size 1259
---
+++ frontend/lib/models/product_list.dart
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:37f29b4e819e9ae1a6f7012f73dbdd26d29b257965c32f00a600474e7dd9495d
+size 792
