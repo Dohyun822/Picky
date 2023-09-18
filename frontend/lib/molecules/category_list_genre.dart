79be2f662c67e910627510c95f6d187dbdcd6a9a
imsongj <imsongj@gmail.com>
2023-09-18 16:53:24 +0900

    feat: 카테고리 선택 시 필터 적용 추가(간편식사, 즉석요리 만)
    Former-commit-id: 02fd35d01e0844fc9821298f2cd3b2cafc7a1813

--- frontend/lib/molecules/category_list_genre.dart
+++ frontend/lib/molecules/category_list_genre.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:63614248af9ad36d7219fd25db3476312927eedd941a1e52b05460a230c3cb51
-size 2241
+oid sha256:334f87998ad43edb94eae6b333f51da73168333b91d38d15b44ca7fb22a4cab5
+size 2514
--- frontend/lib/pages/product_filtered_page.dart
+++ frontend/lib/pages/product_filtered_page.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c63fec723d2238ed9a8afe5581f5882206e84efceeebd4afa49ff228cb7de9e8
-size 1578
+oid sha256:3f7610813243af1439bd439efa3776d9e02499e0ce9121836b84d4236e997f1c
+size 1837
