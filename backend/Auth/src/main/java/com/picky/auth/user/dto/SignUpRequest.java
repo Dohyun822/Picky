53aeb4aba78134d66a9cbaa1d5e13c0c94d96df5
flowerdonk <dbwls5140@gmail.com>
2023-09-18 13:17:14 +0900

    fix: isDeleted default값 수정
    Former-commit-id: 6d9ec2c6aa4c3e6e4f6339d173fa5f5d8717a8b6

--- backend/Auth/src/main/java/com/picky/auth/user/dto/SignUpRequest.java
+++ backend/Auth/src/main/java/com/picky/auth/user/dto/SignUpRequest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cb182d177b06ecc2ba6344f1c4cf1dd98886d48eb363fa726d6ae55da41822a1
-size 1060
+oid sha256:3c351d715b285c6a503d40be75374dd69ce950a556e4e787b909fc561b251380
+size 916
