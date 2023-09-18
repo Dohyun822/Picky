63a089f3943fc6b58c1a28642e34b4b5c6d453f2
flowerdonk <dbwls5140@gmail.com>
2023-09-18 13:22:42 +0900

    fix: 회원탈퇴 수정
    Former-commit-id: 709c9b9f3ed66eb38d218a97f0d4319ee4a51eab

--- backend/Auth/src/main/java/com/picky/auth/exception/ExceptionCode.java
+++ backend/Auth/src/main/java/com/picky/auth/exception/ExceptionCode.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:471946dedc3a23aac6f4e1a27306f22b409fd23a4d3435736f8f688f68eda485
-size 941
+oid sha256:3f1f377f40d376d64793cd758d77b4f6082f9f2adf1872b0742e82cbae2db710
+size 1013
