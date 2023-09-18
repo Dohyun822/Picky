b057daa4e8794920e3bf9932b756b7726998cff4
김기헌 <cbj2741@naver.com>
2023-09-18 13:24:46 +0900

    fix: Product Entity Builder에convenienceCode 추가
    Former-commit-id: b0c75e9cd925fae8b988449890500d1dc017c2c0

--- backend/Business/src/main/java/com/picky/business/product/domain/entity/Product.java
+++ backend/Business/src/main/java/com/picky/business/product/domain/entity/Product.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2c61fd587344464b64e918a35badced305b86649585534cf8c817dd9c49dcb13
-size 2485
+oid sha256:b521b3afbe8ce1e25b9f56c159541aceedb215d04e52b7d30d979e7497e82c60
+size 2452
