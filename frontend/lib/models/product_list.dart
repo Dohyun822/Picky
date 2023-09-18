9633bc9fc1bdcde464ba887dfd47cf63ed0a23de
imsongj <imsongj@gmail.com>
2023-09-18 17:35:47 +0900

    feat: 상품 목록 전체 개수 표시 추가
    Former-commit-id: b4f17f8c251efdff26c421e1b3a694ef948cfb37

--- frontend/lib/models/product_list.dart
+++ frontend/lib/models/product_list.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:37f29b4e819e9ae1a6f7012f73dbdd26d29b257965c32f00a600474e7dd9495d
-size 792
+oid sha256:3f22153dfe362c02f834a269f5f000c9aaa0bf1b515ab4627b85e7dafb0de031
+size 1600
