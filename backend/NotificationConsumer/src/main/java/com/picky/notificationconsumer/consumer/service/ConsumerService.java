aa0db00513cb77495e802141ac9d414487d5e08d
flowerdonk <dbwls5140@gmail.com>
2023-09-19 14:00:24 +0900

    fix: kafka 수신 메세지 형식 수정
    Former-commit-id: 963cd25eac4b15379e4ca63aa93b9e62d2fd65b1

--- backend/NotificationConsumer/src/main/java/com/picky/notificationconsumer/consumer/service/ConsumerService.java
+++ backend/NotificationConsumer/src/main/java/com/picky/notificationconsumer/consumer/service/ConsumerService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fecf1e7ed65a5f1e01f598aee7e29ca227c9997166802033e9e5643d3ba47837
-size 773
+oid sha256:4c95d6bcbab7bfafed89b96714e1a1278e3d36e66c05cc36d1f10c9e5b91d4be
+size 867
