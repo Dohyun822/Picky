76cf3dd3d7b5082081a702e07167547d789330d2
flowerdonk <dbwls5140@gmail.com>
2023-09-19 15:11:00 +0900

    fix: kafka 알림 수신 형식 수정 (Object -> Map)
    Former-commit-id: 649cd039315b0bc7cb6c7d6d1048715bcb7dfd61

--- backend/NotificationConsumer/src/main/java/com/picky/notificationconsumer/consumer/service/ConsumerService.java
+++ backend/NotificationConsumer/src/main/java/com/picky/notificationconsumer/consumer/service/ConsumerService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4c95d6bcbab7bfafed89b96714e1a1278e3d36e66c05cc36d1f10c9e5b91d4be
-size 867
+oid sha256:42986a673fdec5f1b9e2d9111ae488d4f5016fc5f19e5f7951e35fb5dba7ca90
+size 902
