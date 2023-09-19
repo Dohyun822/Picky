b2b759cd7ec253926b35734816e4d460ff831513
imsongj <imsongj@gmail.com>
2023-09-19 16:11:54 +0900

    feat: 라우터 수정
    Former-commit-id: 610594b3b8bf9b4230c017d90db70dec27fc1e08

--- frontend/lib/atom/product_card.dart
+++ frontend/lib/atom/product_card.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:13dbae2f4dd72a99ad6038365ba9b0d223e1d3f2c075565c7da79f068c6875ab
-size 1623
+oid sha256:29fe9e820cc016e656bfda32a5b63474e61d78e24789f67f77accefde8546219
+size 1696
--- frontend/lib/molecules/plus_nav_bar.dart
+++ frontend/lib/molecules/plus_nav_bar.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3545c4122a31d855275e0f5bac4a5d894cfd3a2d3936a46cde4c15195e9df9a1
-size 2488
+oid sha256:6bb66055a0f946dc2cb6c4a2622e3088eca0fbf2e798ffb75bc880de77bab680
+size 2483
