0f92c20ef58f8266a475e21ab3303ad98cf71271
imsongj <imsongj@gmail.com>
2023-09-19 17:00:00 +0900

    feat: fix path names
    Former-commit-id: a0944943fedf1d8d24793bc5b4af1a7d9072ba28

--- frontend/lib/molecules/vertical_more_list.dart
+++ frontend/lib/molecules/vertical_more_list.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cb0caa553646526d14a23b248d5e5068a11b44f95e6cea5d39ac2effbe751a6b
-size 1734
+oid sha256:de856c893fe290b9514c20d0f4b801baac560f634be27138b53405a4bd8e48f0
+size 1731
