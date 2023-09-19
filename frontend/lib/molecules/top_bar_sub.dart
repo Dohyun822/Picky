2d94b702473d7cc1b343e51931d548eaef471649
imsongj <imsongj@gmail.com>
2023-09-19 16:51:36 +0900

    feat: fix path names
    Former-commit-id: 7f113d609d65e461e4e156355245920bdc286bbc

--- frontend/lib/molecules/top_bar_sub.dart
+++ frontend/lib/molecules/top_bar_sub.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0f8e7da59c6ba66a58bafa8355e4cf377f321e836bd5efa2f22e8ae0ded42675
-size 1003
+oid sha256:993a648d2be05902c481e3a7bbe0c8d4760624154fd30334179789b168d80dfb
+size 998
--- frontend/lib/pages/combination_detail_page.dart
+++ frontend/lib/pages/combination_detail_page.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:98e100332de068154bbcdf0646df1af78905afcc4b6cfa49ddbb9f2fb7635897
-size 6445
+oid sha256:0a055b9f4f8323f88e31152fd93a859b2643bda82aa75cf43ffc5ceb25f5106f
+size 6440
