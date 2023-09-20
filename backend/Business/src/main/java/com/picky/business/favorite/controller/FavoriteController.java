93849309bd30856a5e79fd278bf4f0f4ee7f9789
김기헌 <cbj2741@naver.com>
2023-09-20 09:39:26 +0900

    feat: 즐겨찾기 등록
    Former-commit-id: 930c93a51961ece7a2c5d49f100566fb76b17f7e

---
+++ backend/Business/src/main/java/com/picky/business/favorite/controller/FavoriteController.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0caa482598dc5a7a77a65ed96b33ba4a3e78190fce82faba26f1603b96ce7949
+size 1151
---
+++ backend/Business/src/main/java/com/picky/business/favorite/service/FavoriteService.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9de6732f930e54d0628972c7229b0cad39d5662a5bb8b7ef3c372e953c57c146
+size 953
