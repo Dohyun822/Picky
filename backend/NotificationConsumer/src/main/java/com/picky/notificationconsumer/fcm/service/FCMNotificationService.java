8831e4d340ac098997e4bef441e4e0bd3ebac702
flowerdonk <dbwls5140@gmail.com>
2023-09-20 18:02:20 +0900

    chore: FCM kafka 에러 수정 중
    Former-commit-id: 0b8ece3729de0e771ecba376379b816f5563ab25

--- backend/NotificationConsumer/src/main/java/com/picky/notificationconsumer/fcm/service/FCMNotificationService.java
+++ backend/NotificationConsumer/src/main/java/com/picky/notificationconsumer/fcm/service/FCMNotificationService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:13a2df2a9f62c89b4f1ec925575b7543bf0174cefd7c83afaef30500b5da606f
-size 2010
+oid sha256:7fa3203319dc73f1bcd29647fd76f230895caa30477a9a4b101a74992c0fab6c
+size 2243
