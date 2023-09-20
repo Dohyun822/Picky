0ec5c0047062435b4c74767725320039f4ce7ae2
imsongj <imsongj@gmail.com>
2023-09-20 09:37:35 +0900

    feat: 상세 페이지 뒤로가기 버튼 수정
    Former-commit-id: 36943c6269c5c3f925d81ecb4043e663a7e1d706

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e30de0c07f41d1e2bcc4bdb75e1e45678481ab86f47d79a7bc4cb8b70711d4d3
-size 11089
+oid sha256:9c960414544ac3d0e62482a311cb422ce99ebc7d7b28eae5d70064d809472470
+size 11224
