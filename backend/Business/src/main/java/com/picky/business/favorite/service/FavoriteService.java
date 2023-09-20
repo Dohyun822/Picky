359b0874a1401553d93ba032493eaa400b09a6d3
김기헌 <cbj2741@naver.com>
2023-09-20 09:57:48 +0900

    feat: 즐겨찾기 목록 조회 by userId
    Former-commit-id: 845f8003ef0348303e6e21ffc4083d7dbfb3302a

--- backend/Business/src/main/java/com/picky/business/favorite/service/FavoriteService.java
+++ backend/Business/src/main/java/com/picky/business/favorite/service/FavoriteService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9de6732f930e54d0628972c7229b0cad39d5662a5bb8b7ef3c372e953c57c146
-size 953
+oid sha256:69e269e6d0e819010e8bdf915b0ea2adf7ca150ec75cf6ed25d6bec9c9d7668b
+size 2052
