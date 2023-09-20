07e428dd1db16473e14a80c41f34f7c136084783
김기헌 <cbj2741@naver.com>
2023-09-20 17:47:32 +0900

    fix: Product List, Detail시 좋아요 여부 및 로그인 안했을 때 처리
    Former-commit-id: 337964c13efd31d4311598ff1de774f433b16cb6

--- backend/Business/src/main/java/com/picky/business/favorite/controller/FavoriteController.java
+++ backend/Business/src/main/java/com/picky/business/favorite/controller/FavoriteController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f7ba068311ccd46c8c178cbcf7f14912518cf39b718dcb2a49b5fdabade227a5
-size 1601
+oid sha256:a9d8d9ede152693322c4795282d631ac3469eda3e5382527b8193d249a7a1591
+size 1755
--- backend/Business/src/main/java/com/picky/business/product/service/ProductService.java
+++ backend/Business/src/main/java/com/picky/business/product/service/ProductService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b57e51ec44983761a217c47fad7d20720571bbbe035d6ba3d42f32cac7d18ee6
-size 7458
+oid sha256:adf27f05fea8f6f9b0499d2190158e87acc5cfa483104e83980445874f62120f
+size 8952
