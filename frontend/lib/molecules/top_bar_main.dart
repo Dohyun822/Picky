2d5e6b6ac459298350ccd9eaa9975c27dc43b55d
jeongmo <jeremy0410@hanmail.net>
2023-09-20 17:32:40 +0900

    feat : link server
    Former-commit-id: acff2bd96d6b751901893f1dc684765c39d54d90

--- frontend/lib/molecules/top_bar_main.dart
+++ frontend/lib/molecules/top_bar_main.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:81c7eb8814cd7b65898957bf75f360a2a0f3f3d6ab043a2e4eaf47c58295d962
-size 1323
+oid sha256:823ab68c1488eeec331ade723aac397d97ee95db36809631dd60c980e223753c
+size 1407
