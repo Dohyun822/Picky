ab0459a858709419e1eea2fc80a9defb66583eaf
김기헌 <cbj2741@naver.com>
2023-09-20 15:28:05 +0900

    fix: 댓글 수정, 삭제 시 작성자와 현재 로그인한 유저가 같은지 검사 로직 추가
    Former-commit-id: be8ef7273167daa5aa8e18063fa52209e8ba8b43

--- backend/Business/src/main/java/com/picky/business/product/controller/ProductController.java
+++ backend/Business/src/main/java/com/picky/business/product/controller/ProductController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ed939fe69e47387d62d8dfbc458a88d42ad5c71a927a3b76414f0a7569365513
-size 3824
+oid sha256:83bca8ff918abbf57d5e90fa796d5abd15e647eaab4304d729e72a922e46b86a
+size 3856
--- backend/Business/src/main/java/com/picky/business/product/service/CommentService.java
+++ backend/Business/src/main/java/com/picky/business/product/service/CommentService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3cd460cf1fc1e66a3921a912540a926826495257abc3abf7c9964803709d06cb
-size 2835
+oid sha256:9bd863fe9e553e308dd92129ef007cbef14c21222e12a2423172fe9dd3a7ff99
+size 3296
