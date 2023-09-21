1d337191a02e6d19229ae93214cb5343757377ac
jeongmo <jeremy0410@hanmail.net>
2023-09-21 17:42:22 +0900

    feat : network setting
    Former-commit-id: 454ca6a4e06a6250728307fd0b289b261e912ba7

--- frontend/lib/pages/product_detail_page.dart
+++ frontend/lib/pages/product_detail_page.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:440142136d7651ae822502a14cda4f80acec882b5d5b7d046109b274644b6f62
-size 10084
+oid sha256:c6edd87791b776c16e60b23ed4c2e74ecaca08d5b7c07d1b4407d2ec1692cac1
+size 9992
