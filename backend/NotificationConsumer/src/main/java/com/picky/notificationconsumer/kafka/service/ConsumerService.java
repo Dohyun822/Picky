72d78f9fd965ce67a0924eeb0e5ab1e1584937ac
flowerdonk <dbwls5140@gmail.com>
2023-09-21 16:33:59 +0900

    fix: kafka 통한 전달 메세지 타입 수정
    Former-commit-id: eb404cd72fb93345876ecfc3cff381e45b1537ba

--- backend/NotificationConsumer/src/main/java/com/picky/notificationconsumer/kafka/service/ConsumerService.java
+++ backend/NotificationConsumer/src/main/java/com/picky/notificationconsumer/kafka/service/ConsumerService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:106cff0a987c6181a837cde4e774fd2a7224627c0997fc2bff25672e03c323bf
-size 1357
+oid sha256:48f33df6b8c10373c21629cbb3e2629082b7f8ad01176bcb195aff7ca42777ea
+size 1490
