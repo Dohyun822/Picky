b9c2e5ac3a3f1a4224e996ddfb0be1f41c10b8ed
jeongmo <jeremy0410@hanmail.net>
2023-09-22 13:31:55 +0900

    fix: rankinglist revise
    Former-commit-id: d5ad4d1f0318c181f37c175dd87aa88f1fe62401

--- frontend/lib/temp/temp_product_detail_page.dart
+++ frontend/lib/temp/temp_product_detail_page.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8e9408e08f5bad2ddf0bb47a8429ff03b6a253e2885ea3b5ba2491ec78eef088
-size 7003
+oid sha256:a829712ec8cc94cc621f1cdd689d6ae105d9aff0991e2a864524182de5549190
+size 7006
