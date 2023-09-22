0e035c3b22d76f9c588a872688dbbb4698c2e412
imsongj <imsongj@gmail.com>
2023-09-22 13:33:50 +0900

    feat: 상품 목록 api 연결 테스트
    Former-commit-id: a5a72b820ff5407fba04a3eafe0b36741f099c0a

--- frontend/lib/molecules/vertical_list.dart
+++ frontend/lib/molecules/vertical_list.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d1f607057dfb8583e6695764f41b26607a6c3eeb484dd62243ee83ab134ab8c9
-size 2274
+oid sha256:7bb8dbfbea5fae1bb0a752426ab52d4d66804aabd08c9166fc8ca8492ba362f0
+size 2370
