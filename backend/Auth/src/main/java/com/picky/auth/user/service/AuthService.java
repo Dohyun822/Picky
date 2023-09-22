5b572188d0dc0e919ae075c321f76ffff905d7ab
flowerdonk <dbwls5140@gmail.com>
2023-09-22 12:52:28 +0900

    feat: 유저 회원가입 시 fcmToken 저장
    Former-commit-id: 4924baceb08dfe98fdf642222b33112c396f1c8c

--- backend/Auth/src/main/java/com/picky/auth/user/service/AuthService.java
+++ backend/Auth/src/main/java/com/picky/auth/user/service/AuthService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:704e87d48ceb849f5dcbde6466fc1264c53e85971d3d0eeeff55db515fc1f317
-size 4547
+oid sha256:639b181d67e2c9a49ecbd697a66d78e35739e41f98c9af69ee089a4d61751e2f
+size 4600
