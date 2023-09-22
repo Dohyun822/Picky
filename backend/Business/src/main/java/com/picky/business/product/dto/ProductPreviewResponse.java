db3fc015926297c18836ad8d6c5f6f52059520dc
김기헌 <cbj2741@naver.com>
2023-09-22 14:25:26 +0900

    fix: badge 삭제 및 모듈화
    Former-commit-id: 8e9dd18e125f2ffb1538f803e39150448d047651

--- backend/Business/src/main/java/com/picky/business/product/dto/ProductPreviewResponse.java
+++ backend/Business/src/main/java/com/picky/business/product/dto/ProductPreviewResponse.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:190c001c673dcb097883675a4a665d2a3bf3daf8d3fa13b809717f10b117e0c1
-size 1055
+oid sha256:b49df6170b585b3006af4fa11a8d48fd5fef2542f6aef803bf8091f1d08f7533
+size 624
