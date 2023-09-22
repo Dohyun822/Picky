63f7c312ffdfd52c96b889a3b6727052123901ad
imsongj <imsongj@gmail.com>
2023-09-22 17:50:14 +0900

    feat: 상품 이름 버튼 추가
    Former-commit-id: d26f4571313f25f7c3c3f157e2b1562438d3dd7c

--- frontend/lib/molecules/combination_list.dart
+++ frontend/lib/molecules/combination_list.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0507df406550a1c505eaa82183f020810ac27e39a434fed73d008c36acf78114
-size 1435
+oid sha256:2f7cd7bf6333bddcf68967d0e8811bb57f9b5a51ad7d351c027707f2d34d15e5
+size 1525
