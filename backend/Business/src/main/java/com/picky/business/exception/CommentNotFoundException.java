3bc6e3ccd295594a02db919c6cff13acca7162ea
jeongmo <jeremy0410@hanmail.net>
2023-09-25 10:08:19 +0900

    fix: product_list_simple model fix
    Former-commit-id: 745c061c2ec6f53b2821f382c37cad4a9e9ffcfa

--- backend/Business/src/main/java/com/picky/business/exception/CommentNotFoundException.java
+++ backend/Business/src/main/java/com/picky/business/exception/CommentNotFoundException.java
@@ -1,3 +1,16 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:13c0f01f9fe0f305bbc48b37e5863fdd6c4193b83df4ebffc9dbaac3562275f2
-size 384
+package com.picky.business.exception;
+
+public class CommentNotFoundException extends RuntimeException {
+    public CommentNotFoundException(String message) {
+        super(message);
+    }
+
+    public CommentNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public CommentNotFoundException(Throwable cause) {
+        super(cause);
+    }
+}
+
