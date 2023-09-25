e76c44875100cdf6f9655aa68b7babd224590f3b
김기헌 <cbj2741@naver.com>
2023-09-25 10:26:16 +0900

    fix: convenienceCode, promotionCode 's' 추가
    Former-commit-id: 55ac03ef7bd7132d8683ea1e38ca54159a6a437a

--- backend/Business/src/main/java/com/picky/business/product/controller/ProductController.java
+++ backend/Business/src/main/java/com/picky/business/product/controller/ProductController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8d98aa4abdcbf16530e37a3b15e520b4675a5f27dbeeba964961ca0ce24fd5d2
-size 4134
+oid sha256:d0a287b43668ee855d397cc4d060cefa391a0662e832cb3f91c5d836a6d86aef
+size 4138
