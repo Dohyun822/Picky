7c59b7d0752fe260f5092e50da849fdee2405142
imsongj <imsongj@gmail.com>
2023-09-25 10:30:25 +0900

    feat: 필터 수정
    Former-commit-id: eea616c8f11f1401b7ceeb4d192e2bf2efcba1f7

--- frontend/lib/models/filter.dart
+++ frontend/lib/models/filter.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:095e42c86176ceab7b8ede3d39197125bf91879682cc0e25ae2a9c8d355db7fb
-size 2216
+oid sha256:43803a97b1c11838b7e7d6e5908e4b364c899ca2a9d15f534e0a8b503cf69b4b
+size 2292
