c9387f79cd6ac4d0ce4c537b56b8e80b2a4aa154
jeongmo <jeremy0410@hanmail.net>
2023-09-26 12:48:49 +0900

    feat: random 오늘의 할인 상품
    Former-commit-id: f0943fd6bfb86b50950a1fff87bf12247979297d

--- frontend/lib/pages/home_page.dart
+++ frontend/lib/pages/home_page.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bd47ed1a97c614c576a61716ff3b7a24da8c6464e4c5bff4fbba2c954313de2b
-size 1192
+oid sha256:727bf66ec07da43431389beb1cf68c3c756dbdfe16fe218c2555e6c7c6ac4947
+size 1169
