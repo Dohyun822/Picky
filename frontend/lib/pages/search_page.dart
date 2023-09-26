02150ca2bf605088cb290375316844a4508a55e6
jeongmo <jeremy0410@hanmail.net>
2023-09-26 14:20:01 +0900

    fix: search page fix
    Former-commit-id: b4c30c678828ecb6c521263f72dfe78e5425fa5c

--- frontend/lib/pages/search_page.dart
+++ frontend/lib/pages/search_page.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:38222eaeef55aeb2db13fef490d2fae144aa22d77ea02fb6f41b4b14a6d183ca
-size 8788
+oid sha256:de8d5fe7ce2e9da97e21e7bf96f336a99fed291543c6ae1cc169b6898018330b
+size 8649
