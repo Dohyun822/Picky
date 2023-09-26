7992cf4f16831eba0d3cf7da6c8e2bf87a721811
jeongmo <jeremy0410@hanmail.net>
2023-09-26 09:27:25 +0900

    feat : combination page
    Former-commit-id: 407cbb3a147910ea2adb68605c0d6511c2d0c44d

--- frontend/lib/pages/combination_detail_page.dart
+++ frontend/lib/pages/combination_detail_page.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e09431bdc1a8c3ffea6f4b8c7aa99ec93b8bf75b27cce964a2fd271683afdafa
-size 5882
+oid sha256:7a0026901935d236b66b0743ecde1f28ac4aa2e4e63af499e6025a881cb3bdc1
+size 5898
