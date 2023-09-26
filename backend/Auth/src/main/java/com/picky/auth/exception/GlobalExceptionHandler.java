a2dfbe39046f36637239aa9215c2f450c2a9ca11
flowerdonk <dbwls5140@gmail.com>
2023-09-26 17:49:19 +0900

    fix: 예외처리 추가
    Former-commit-id: 17ed4a61d842698ac94a6227c4577898c6468bd5

--- backend/Auth/src/main/java/com/picky/auth/exception/GlobalExceptionHandler.java
+++ backend/Auth/src/main/java/com/picky/auth/exception/GlobalExceptionHandler.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7d138fef8842a47dbf798031f0735fced372a990a097b05e8e7dccf3b7050f93
-size 644
+oid sha256:24c934962ed203d941ae629171c0dcd0487160ceb67e1c10bb14f7857a405a46
+size 1238
