8d939fbd36386adc1b95e688b0498f0fe848a128
jeongmo <jeremy0410@hanmail.net>
2023-09-26 14:16:13 +0900

    feat: serach page update 검색창에서 이동 및 검색어 이동
    Former-commit-id: e8f34d2701b40b333d3220b018d586cce543a903

--- frontend/lib/molecules/vertical_more_list.dart
+++ frontend/lib/molecules/vertical_more_list.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8af410565b9ee9bfe052fcbbf877789cb21c6acfe76d042aa348799173f23e2e
-size 3526
+oid sha256:707d7b001cba4b7086b1b39ee5612fd483677395dcda44771b4f3eeff87b0b43
+size 3560
--- frontend/lib/pages/search_page.dart
+++ frontend/lib/pages/search_page.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2b209da564e6613023664202452a171ba81bfcb34ca98828c72ab3d7f7d9fc6c
-size 7636
+oid sha256:38222eaeef55aeb2db13fef490d2fae144aa22d77ea02fb6f41b4b14a6d183ca
+size 8788
