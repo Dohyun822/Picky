929fea0e31ae1a3c133845694e3c5edb91034d65
flowerdonk <dbwls5140@gmail.com>
2023-09-27 09:10:26 +0900

    chore: batch server 삭제
    Former-commit-id: e2159e7e114adc7262484d6b3d1029e201f29f0d

--- backend/Batch/src/main/java/com/picky/batch/BatchApplication.java
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:cbfd5db44b995927db69de0cb8308b757f15520cd950ebb86f54b97fa00617f3
-size 306
--- backend/Batch/src/test/java/com/picky/batch/BatchApplicationTests.java
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:d8c65200f5c411cf7f7510b21bea75ce7f38d01c89e961952dc7410eaf248fc3
-size 206
