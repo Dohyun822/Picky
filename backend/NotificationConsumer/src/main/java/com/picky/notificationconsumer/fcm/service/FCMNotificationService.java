0ca2d98a2ddc677bce341576ec4a5616b75380fd
flowerdonk <dbwls5140@gmail.com>
2023-09-27 09:32:11 +0900

    fix: 더미데이터 전용 로직 추가 (null 처리)
    Former-commit-id: 127213a7215c3f4f9c4fba43b198d76192f42b18

--- backend/NotificationConsumer/src/main/java/com/picky/notificationconsumer/fcm/service/FCMNotificationService.java
+++ backend/NotificationConsumer/src/main/java/com/picky/notificationconsumer/fcm/service/FCMNotificationService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:840a2c06b8d00885917438ee07a9d8e6093b1fbb9a61f6f2c168e331e59e7abe
-size 3047
+oid sha256:cca2829470bc8b1db48d2461674347a1e7ce205470910ef3dad30caf4473a153
+size 3275
