dd2b2e6c60316b2a12a0b9c6d505976aa139c514
imsongj <imsongj@gmail.com>
2023-09-27 14:14:21 +0900

    feat: 알림 추가 (com.ssafy.picky로 변경)
    Former-commit-id: 52dd32ff8672b301ab4ea7eef439e33029c2cd70

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a15f664689e11eb1b9e0d60a2379abd48d119249da5c478fe35525595d96496c
-size 11681
+oid sha256:cc0669147d58dd072fc7ba9a982834ec666b6c9ea3c1a1805e675df9b7345ced
+size 10098
--- frontend/lib/pages/scrapbook_page.dart
+++ frontend/lib/pages/scrapbook_page.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f542f6eed71ebea2577b10d1155dc04721e6d8d49f5af61b10e59213ae9669eb
-size 6441
+oid sha256:d481fed33ad16b20d722bedc1228a9efeb516f691a081f362156007589d62933
+size 6442
