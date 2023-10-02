f6286fb66153cd6d728b732fa2fdf0c76b871173
imsongj <imsongj@gmail.com>
2023-10-02 23:46:43 +0900

    feat: 조합 디테일 페이지 수정
    Former-commit-id: 666612a1d1977ebe39299967e993ce2588813a93

--- frontend/lib/atom/product_card.dart
+++ frontend/lib/atom/product_card.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8739bea02de59d4bb7a35a4208be9a8797dcca8c9d2df668ef26727908a313b4
-size 1853
+oid sha256:d4f93dcef3bc95e3c89b0ee4c0d3eeb60c7988485222ddf75969f73e2cd3dd7e
+size 1776
