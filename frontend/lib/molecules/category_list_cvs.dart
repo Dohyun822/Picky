0c96cbde7587a68c1dc5d5b53b7966d204d827ed
imsongj <imsongj@gmail.com>
2023-10-03 03:52:59 +0900

    feat: 목록 가격 별 정렬 구현
    Former-commit-id: 174bafcb9773405ee62e3524596921b5799b9504

--- frontend/lib/molecules/category_list_cvs.dart
+++ frontend/lib/molecules/category_list_cvs.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:76a82c2c348a0ee97d4193b4c5008dccafc1ccd9b5699be21d4b67d02b7075ef
-size 1739
+oid sha256:7f6c4232657041ced882db8e09d993499f4d083f40ff36be67df9c9f22a7fd31
+size 1728
