907eb97c152a768353d07b4f816c0f85b82cf4ea
imsongj <imsongj@gmail.com>
2023-10-04 18:09:36 +0900

    feat: 차트 수정
    Former-commit-id: 5ad71cc82fcd61ac312de1d5965c66b683d11b3d

---
+++ frontend/lib/molecules/combination_chart.dart
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:292a0901ea9499f3c09f7e8788bcf8bbbf0b759f4f8932f779a447334eb9ae7a
+size 13670
--- frontend/lib/pages/combination_detail_page.dart
+++ frontend/lib/pages/combination_detail_page.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:98465ef35a0779f22f437563463126c17442d410bc8760bc1b9800eaddb04dc4
-size 5049
+oid sha256:7ea97adb9c9f5d4229ba1a8543502cdfaf4c33de1cb1461e94e8cb9b03f08e9d
+size 5443
