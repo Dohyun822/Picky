be3ea5c3a4c8414e4c5c423519c92f016cd79093
imsongj <imsongj@gmail.com>
2023-10-04 14:44:23 +0900

    feat: 페이지네이션 추가
    Former-commit-id: f9a9f5a0de4b46355f4fdaec56229d95a686ea27

--- frontend/lib/pages/login_page.dart
+++ frontend/lib/pages/login_page.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:95e50f36b104c96ea1576b0276852977ab7a30b4f81a3f6d979530b954973a70
-size 4553
+oid sha256:e6195c04d131a8d7a9c827e262fcf8e78f5ead165cf17c9f492232ff988b344c
+size 4876
--- frontend/lib/pages/signup_page.dart
+++ frontend/lib/pages/signup_page.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0ecf9defbf2cf3263dd212d31da2629429480b439a4d975c5575aa347357afc7
-size 4661
+oid sha256:94c3b46b2f360627fb09e9a836f2379297266c16ac081537ad3b77f005a4765b
+size 4913
