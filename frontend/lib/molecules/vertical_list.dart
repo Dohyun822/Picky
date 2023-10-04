17d9c4e499e210e378d9e2aa162e6f743a874e35
imsongj <imsongj@gmail.com>
2023-10-04 14:44:23 +0900

    feat: 페이지네이션 추가
    Former-commit-id: b65639521cbc3baf94013f97f9f1675991a113cd

--- frontend/lib/molecules/vertical_list.dart
+++ frontend/lib/molecules/vertical_list.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ec249fd616725cea72ffe4a51d1777ed8b0f11e0d29aedb7d494d5f4575b0576
-size 5559
+oid sha256:20eef4c2151f54cd28b85a51973644d102a9559eac280f66f9d16bcea8d992b2
+size 6039
--- frontend/lib/pages/cart_page.dart
+++ frontend/lib/pages/cart_page.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:06421a32fc702e625e4db83c39d37b258de1a08478c4900dd1690f1aaf90c483
-size 6407
+oid sha256:1c7d8e0b20ebf63a78aae9043dab9682dac62b60672f88c2038e898853f25374
+size 6268
