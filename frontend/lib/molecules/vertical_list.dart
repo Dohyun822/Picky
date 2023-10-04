aa5ba0c098545f20ee7c7a08bb4e1551416caf06
imsongj <imsongj@gmail.com>
2023-10-04 18:09:36 +0900

    feat: 차트 수정
    Former-commit-id: 995bafe0e1fb3691b78cb0117932d0ec8e38ad75

--- frontend/lib/molecules/vertical_list.dart
+++ frontend/lib/molecules/vertical_list.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a83f6e7f96fda9e6a4b3ae33ef2eb2a0f31a61355d9abafd5c8a93484885fed2
-size 6037
+oid sha256:b7dc9ed70b2a4bcc5c4850f18adc890bdce64b2f4678e145eb78f68ed1586e67
+size 6038
