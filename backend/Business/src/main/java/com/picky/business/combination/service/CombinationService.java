8a08f98e79ee3473ee88f530d08ad0d44a6a7565
김기헌 <cbj2741@naver.com>
2023-10-04 14:30:41 +0900

    chore: logging 추가 및 createdAt 설정
    Former-commit-id: 6029487d66f8e8a995fde7c411eb02853a4d7dba

--- backend/Business/src/main/java/com/picky/business/combination/service/CombinationService.java
+++ backend/Business/src/main/java/com/picky/business/combination/service/CombinationService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c6050e4009c1cc39f43e97c9136da2d6641f9c142b00480cca07c7eeaf4e4755
-size 7196
+oid sha256:afc47b97d0b379ddaa444791a71e51e39d9985c3ebece18206d9ca0429b97994
+size 7357
