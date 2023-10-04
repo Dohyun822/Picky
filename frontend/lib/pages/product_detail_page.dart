bf7359b9392a8dc8f7fdcd732cabb337a6527041
imsongj <imsongj@gmail.com>
2023-10-05 03:23:26 +0900

    feat: 차트 디자인 수정 및 통일
    Former-commit-id: cc03eea03d54abe9656c63a86cb3f8353fc512e4

--- frontend/lib/pages/product_detail_page.dart
+++ frontend/lib/pages/product_detail_page.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ebaaf82637f511e90a6364413e0cbc2bd435b9a7afe35e3c02395fb0f61daeb4
-size 7530
+oid sha256:6dffc2e8abc6016b0286bdf7dc370bb75ef0afd296a045414294b71cc93bd97f
+size 7822
