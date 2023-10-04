ecb6645909b36d76dac52a7e2fff6577bcc889b6
flowerdonk <dbwls5140@gmail.com>
2023-10-04 13:03:07 +0900

    chore: 불필요한 import 삭제
    Former-commit-id: c87e83d5fcc8669532b87ba57252fe20920d9c4f

--- backend/NotificationConsumer/src/main/java/com/picky/notificationconsumer/fcm/service/FCMNotificationService.java
+++ backend/NotificationConsumer/src/main/java/com/picky/notificationconsumer/fcm/service/FCMNotificationService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2b23456d58f8e4eaaafd22d7590cb3241d5bfea6c47663355dfd4301bcd01989
-size 3890
+oid sha256:7fb06b4ca59b7780c94112725061fd2531750ccb47aa312d88618bf058b5dcc9
+size 3825
