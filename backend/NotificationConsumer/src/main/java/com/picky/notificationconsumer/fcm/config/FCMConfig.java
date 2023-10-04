6e1a991381c1e84663239bddfbe480f8130b706f
flowerdonk <dbwls5140@gmail.com>
2023-10-04 13:02:31 +0900

    fix: fcm 수정 테스트 완료
    Former-commit-id: a4edf4bd8d35621f12574195b501535c11d29f45

--- backend/NotificationConsumer/src/main/java/com/picky/notificationconsumer/fcm/config/FCMConfig.java
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:bccc59123f3ea29a07ae25657ba39a78fee2128deb50880c2d527498da42922e
-size 2036
--- backend/NotificationConsumer/src/main/java/com/picky/notificationconsumer/fcm/service/FCMNotificationService.java
+++ backend/NotificationConsumer/src/main/java/com/picky/notificationconsumer/fcm/service/FCMNotificationService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:304f02411f91a82238de2dabb60064dca8df0b5a29b82bd5700c74854d7d5330
-size 3263
+oid sha256:2b23456d58f8e4eaaafd22d7590cb3241d5bfea6c47663355dfd4301bcd01989
+size 3890
