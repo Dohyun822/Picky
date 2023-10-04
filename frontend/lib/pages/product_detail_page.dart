88b2761f09918b4808b53a8f027aabe45493d008
imsongj <imsongj@gmail.com>
2023-10-05 04:28:04 +0900

    feat: 오류 수정
    Former-commit-id: 9142c2ef4b7f554a7c51bfe685010c64fca4dd1c

--- frontend/lib/pages/product_detail_page.dart
+++ frontend/lib/pages/product_detail_page.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6dffc2e8abc6016b0286bdf7dc370bb75ef0afd296a045414294b71cc93bd97f
+oid sha256:842f8f2ab1813870dd2a1545477e7e7b1d7ce65ba1535ad05a8fca726996987f
 size 7822
