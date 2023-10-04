c31ab2e60d22473f7cc38aadb1ab60a987a91534
김기헌 <cbj2741@naver.com>
2023-10-04 10:06:25 +0900

    fix: favorite 삭제 조건 변경
    Former-commit-id: 3584c5e5421657a02f28ad8afc7debe3b10b749e

--- backend/Business/src/main/java/com/picky/business/product/service/ProductService.java
+++ backend/Business/src/main/java/com/picky/business/product/service/ProductService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:04bf632dd6baeb31c077cd2d6d232fbdda9641855a3b9aa36d06e5cf6ffcff8d
-size 10784
+oid sha256:fef3813fbbd2119792acf15d595fa634dfff86f9c7e35ab7da570c353f542b71
+size 10818
