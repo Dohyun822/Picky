de75fa8d067c0aa6c155fb667704da01f5088be4
yup8229@gmail.com <yup8229@gmail.com>
2023-10-05 14:14:00 +0900

    feature : 영양소기반 추천 api 구현 완료
    Former-commit-id: ad035f59504aad4ad826cb32d6798e8dabe4f00f

--- backend/Business/src/main/java/com/picky/business/recommend/controller/RecommendController.java
+++ backend/Business/src/main/java/com/picky/business/recommend/controller/RecommendController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6b831f2ae407e90c00e62cb5367de935f0f40e9a682d27b2ec5e9da7447f9ce7
-size 1945
+oid sha256:08fa7955a5a4db50900b7e04ae8ba8c0f57c9cae795da400a8bbc122cb6cb8f0
+size 1876
--- backend/Business/src/main/java/com/picky/business/recommend/service/RecommendService.java
+++ backend/Business/src/main/java/com/picky/business/recommend/service/RecommendService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f20f56631f574909c4e77ceea6a5cc872de461a72c40119f36b93c03486223bf
-size 5113
+oid sha256:7d810950a06fd7c7bac0221c8082fbe66081ce754b269f4393cd14fc3e3bc3fa
+size 7388
