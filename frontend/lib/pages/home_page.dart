ae87f242b17571157561f09489dbe76ceeb46de5
jeongmo <jeremy0410@hanmail.net>
2023-10-05 12:22:35 +0900

    feat : before merge
    Former-commit-id: c2a21cd2e829f9493212c7c83f2bdb615d1719be

--- frontend/lib/pages/home_page.dart
+++ frontend/lib/pages/home_page.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:727bf66ec07da43431389beb1cf68c3c756dbdfe16fe218c2555e6c7c6ac4947
-size 1169
+oid sha256:8d564a110811943c77810be792d85668eccc9888a0a484545d4ecd0a50f68ca0
+size 1446
