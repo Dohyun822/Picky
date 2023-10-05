d36c2628f95874cc706b9ee875c92263e60b1814
jeongmo <jeremy0410@hanmail.net>
2023-10-05 12:22:35 +0900

    feat : before merge
    Former-commit-id: 97c4de6d2e8dc202ca4b58363800f05ca38cb9ea

--- frontend/lib/pages/product_list_page.dart
+++ frontend/lib/pages/product_list_page.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:273ce56af5906cf95201be4a737ea66cb8df2032313744af21eaae9b3c2d01ba
-size 1031
+oid sha256:44f4a574db4da48ba3a066c4ba6bb7870f344d19506ef6fbc32cea23210ef031
+size 1298
