3be05f4456dfbe47f63b1d4b6d22b389f0cc7a36
imsongj <imsongj@gmail.com>
2023-10-05 17:02:29 +0900

    feat: 조합 상품 클릭 수정
    Former-commit-id: bd64288cc019d249b2c91689fbdf42f8343e3618

--- frontend/lib/molecules/ranking.dart
+++ frontend/lib/molecules/ranking.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8459b775c4e26b1861fd1fa0da23ff94d9a803e4121e76c3696bba5afc3cf813
-size 3087
+oid sha256:9a673ba6d6646a57af0e5f7d88f2883e5fcdf30cc0212b43775a508ef9b1acaf
+size 3047
--- frontend/lib/pages/product_detail_page.dart
+++ frontend/lib/pages/product_detail_page.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8c45a4ba0cac1d9e6e186ee85cf6dcbc2f8492aa301512a34a0601f1282aceb3
-size 7806
+oid sha256:e895d229a0e14814e4fddb4b3cd2425d3baf44db35956fc6088d5be24ba0e442
+size 7722
