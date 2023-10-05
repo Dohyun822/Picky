76800635fd2e09af711862492aa84b2202112d03
imsongj <imsongj@gmail.com>
2023-10-05 12:24:35 +0900

    feat: 인기검색어 추가
    Former-commit-id: 5fda4d99711bd3d971d61ebdf0665457a41d9135

--- frontend/lib/pages/product_detail_page.dart
+++ frontend/lib/pages/product_detail_page.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:842f8f2ab1813870dd2a1545477e7e7b1d7ce65ba1535ad05a8fca726996987f
-size 7822
+oid sha256:64930b6d2c57f73e256cb5682f8f4c12eef6b0551614baaad2bbadb3287ed8e1
+size 7806
