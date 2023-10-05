a393dafe67e407e8390bdd4220f9dc9dbb74ad47
yup8229@gmail.com <yup8229@gmail.com>
2023-10-05 09:47:29 +0900

    feature : redis설정
    Former-commit-id: 1288a2774dee6992736c8c9dbfa6a83805b40413

--- backend/Business/src/main/java/com/picky/business/recommend/service/RecommendService.java
+++ backend/Business/src/main/java/com/picky/business/recommend/service/RecommendService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ce930c4edbedbad747ff97206213c812df4c9938740a416ae390dc827f8029a5
-size 5035
+oid sha256:f20f56631f574909c4e77ceea6a5cc872de461a72c40119f36b93c03486223bf
+size 5113
