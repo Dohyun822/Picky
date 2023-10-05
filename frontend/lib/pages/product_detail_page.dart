0fed05de282af6b5c1d9c6d05412541f10376bdc
imsongj <imsongj@gmail.com>
2023-10-06 05:28:21 +0900

    fix: 즐겨찾기 오류 수정
    Former-commit-id: c2fc2ca5788e6465d047bf73399735677afac920

--- frontend/lib/pages/product_detail_page.dart
+++ frontend/lib/pages/product_detail_page.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f53ae76abcff605be0f7ef41adde4c0b56477c75db78d0004b9b96b9afa7c3c0
-size 7736
+oid sha256:5842525472c20edb9ee4bab3d7b786dd616b36101bb009e07ae2f555384a4161
+size 8148
