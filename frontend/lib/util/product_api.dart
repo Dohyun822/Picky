af57f8bf0967329488dbe79f646354b1407f9082
jeongmo <jeremy0410@hanmail.net>
2023-10-06 09:08:06 +0900

    feat : almost fin
    Former-commit-id: 68c886d8774bd454aa098aea1a440682a48ef694

--- frontend/lib/util/product_api.dart
+++ frontend/lib/util/product_api.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aee58fc04c2422b9ec934eafb538b8130c220a10c5f37aa201e2fce5cab82f85
-size 6946
+oid sha256:db691f9ca4436a5b7e958afdf21b1a184efcacae8958f97882443f07647fb3e2
+size 6928
