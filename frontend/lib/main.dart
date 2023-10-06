c13ff19f17d3d66505d0f563cb716bcc5a9d271b
imsongj <imsongj@gmail.com>
2023-10-06 10:54:43 +0900

    feat: 오류 수정
    Former-commit-id: 1519aecb10b75f35b731840b3827f830a7f8c142

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b40a21f884d1822fdf44ce1c010323cd0794f2853d723c98aaa92b2d6a3da479
-size 10578
+oid sha256:c08db22bc66a554429d6a954f3e5e2290940f55511cec62970ace91c37cdd95a
+size 10599
