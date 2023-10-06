9ec06e4dcc1aeecafc25ec86a3b91645f594c271
임준성 <jsbryan@naver.com>
2023-10-06 09:35:59 +0900

    ✨ 편의점 코드를 데이터프레임에 따라 정리하는 코드
    Former-commit-id: 3eef3e4991741d6d0b36351832a3fbcc49614331

--- DataSet/CombineAndFix/convCode.py
+++ DataSet/CombineAndFix/convCode.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:634e3406de2a0d9c1dacf0e20859383060ed3feed2abffd3e885ee9a9c2eaad9
-size 826
+oid sha256:df5905c830d4df54c9c91d2c18c46466816244b9a22f6f37cd139a6bf7876399
+size 915
